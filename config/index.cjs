eddfc62e770b1af8b3e1eb3138193e6f94e8a217
一个想入门的小萌新 <93499890+AvadaSm@users.noreply.github.com>
2022-09-29 17:06:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ou3Y56_ln29zvcAygEIsEBWdtDzQ',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '5Y-foaDbRT0qFiV0sM9eUicRHokGBNs9zx5j8gnjHvs',
+      useTemplateId: 'ou3Y56yU2gjdZY-8U-iY6_mC6fzc',
       // 所在省份
       province: '浙江',
       // 所在城市
