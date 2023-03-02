b5ea5b75f67352c358d40b9388cfe243936f8d09
一个想入门的小萌新 <93499890+AvadaSm@users.noreply.github.com>
2023-03-02 17:47:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,17 +63,17 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '方可欣',
+      name: '宇宝老婆',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ou3Y56yU2gjdZY-8U-iY6_mC6fzc',
+      id: 'ou3Y56zKD7VCQn4nto1kjtPZrLeI',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '5Y-foaDbRT0qFiV0sM9eUicRHokGBNs9zx5j8gnjHvs',
+      useTemplateId: 'HVWkSROP2xcrggn5Ha3PxhvXvvxHaMSmp5O29DSz98E',
       // 所在省份
-      province: '湖北',
+      province: '安徽',
       // 所在城市
-      city: '荆州',
+      city: '阜阳',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '09-09',
+      horoscopeDate: '10-15',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -82,20 +82,20 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '刘昊坤', year: '2001', date: '01-23',
+          type: '*生日', name: '坤坤', year: '2001', date: '01-23',
         },
         /*{
           type: '节日', name: '认识纪念日', year: '2020', date: '09-03',
         },*/
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '方可欣', year: '2004', date: '09-09',
+          type: '生日', name: '宇宝', year: '2002', date: '10-15',
         }
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2020-03-04' },
+        { keyword: 'love_day', date: '2023-02-15' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
         // 退伍日
@@ -247,9 +247,9 @@ const USER_CONFIG = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: '江苏',
+  PROVINCE: '浙江',
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: '南京',
+  CITY: '杭州',
 
   /** 重要节日相关 */
 
